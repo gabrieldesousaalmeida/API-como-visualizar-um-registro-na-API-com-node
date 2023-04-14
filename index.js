1740cbe7606c3331e68d70c58dcbbdd63e21b3f9
gabrieldesousaalmeida <74242647+gabrieldesousaalmeida@users.noreply.github.com>
2023-04-14 17:58:58 -0300

    estrutura básica

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const app = express();
+
+//mongodb
+const mongoose = require('mongoose');
+
+// models
+require('./models/Artigo')
+const Artigo = mongoose.model('artigo')
+
+//conectando com banco de dados
+mongoose.connect('mongodb://0.0.0.0:27017/conteudo', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(()=>{
+    console.log('Conexão com banco de dados feita com sucesso')
+}).catch((erro)=>{
+    console.log('erro na conexão com banco de dados'+ erro)
+})
+
+// fazer o express permitir o uso de json
+app.use(express.json());
+
+//listando na API
+app.get('/', (req, res)=>{
+    const artigo = Artigo.find({})
+    .then((artigos)=>{
+        return res.json(artigos)
+    }).catch((error)=>{
+        return res.status(400).json({
+            error: true,
+            message: 'Error: erro na consulta do artigo no banco de dados ' + error
+        })
+    })
+})
+
+// cadastrando na API
+app.post('/artigo', (req, res)=>{
+    const artigo = Artigo.create(req.body)
+    .then(()=>{
+        return res.status(200).json({
+            error: false,
+            message: 'Artigo foi cadastrado com sucesso no banco de dados'
+        })
+    }).catch((error)=>{
+        return res.status(400).json({
+            error: true,
+            message: 'Error: Artigo não foi cadastrado no banco de dados ' + error
+        })
+    })
+})
+app.listen(8080, ()=>{
+    console.log('O servidor foi iniciado na porta 8080: http://localhost:8080')
+})
